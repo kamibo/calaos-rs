b9d79ada19a36ffb246e0bd8f7219b0a5a909fa6
Camille Bordignon <camille.bordignon@gmail.com>
2020-12-02 13:31:23 +0100

    Use clap to handle command line args

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #[macro_use]
+extern crate clap;
+#[macro_use]
 extern crate serde_derive;
 #[macro_use]
 extern crate nom;
@@ -16,7 +18,9 @@ use std::env;
 use std::error::Error;
 use std::net::SocketAddr;
 use std::path::Path;
-use std::time::Duration;
+
+use clap::App;
+use clap::Arg;
 
 use tokio;
 use tokio::signal;
@@ -25,25 +29,41 @@ use tokio::sync::mpsc;
 use tracing::*;
 use tracing_subscriber::FmtSubscriber;
 
+const PROJECT_NAME: &'static str = env!("CARGO_PKG_NAME");
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let arg_io_configfile = "io_config";
+    let arg_rules_configfile = "rules_config";
+
+    let arg_matches = App::new(PROJECT_NAME)
+        .about("Experimental calaos server")
+        .author(crate_authors!())
+        .version(crate_version!())
+        .arg(
+            Arg::new(arg_io_configfile)
+                .about("IO config file")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::new(arg_rules_configfile)
+                .about("Rules config file")
+                .required(true)
+                .index(2),
+        )
+        .get_matches();
+
     let subscriber = FmtSubscriber::builder()
         .with_max_level(Level::TRACE)
         .finish();
 
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
-    let file_io = match env::args().nth(1) {
-        Some(file) => file,
-        None => return Err("Missing file io".into()),
-    };
+    let file_io = arg_matches.value_of(arg_io_configfile).unwrap();
+    let file_rules = arg_matches.value_of(arg_rules_configfile).unwrap();
 
-    let file_rules = match env::args().nth(2) {
-        Some(file) => file,
-        None => return Err("Missing file rules".into()),
-    };
-
-    info!("Start calaos-rs server");
+    info!("Start {} server", PROJECT_NAME);
 
     let io_config = io_config::read_from_file(Path::new(&file_io))?;
     let rules_config = rules_config::read_from_file(Path::new(&file_rules))?;
@@ -81,7 +101,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
       }
     }
 
-    info!("End calaos-rs server");
+    info!("End {} server", PROJECT_NAME);
 
     Ok(())
 }
