f8f0b6d2e1ff7b6372e5bffc42430b12d01b375a
Camille Bordignon <camille.bordignon@gmail.com>
2021-08-31 22:41:16 +0200

    Improve shutter control

--- src/io/wago_modbus_controller.rs
+++ src/io/wago_modbus_controller.rs
@@ -1,9 +1,8 @@
+use std::collections::HashMap;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::net::SocketAddr;
-use std::time::Duration;
-
-use tokio::time::sleep;
+use std::time::Instant;
 
 use tokio_modbus::client::tcp;
 
@@ -14,12 +13,15 @@ use crate::io_context;
 use crate::io_value;
 
 use io_config::OutputKind;
+use io_config::WagoIOUpDown;
+
 use io_context::BroadcastIODataTx;
 use io_context::IOData;
 use io_context::OutputContextMap;
 use io_context::OutputIODataRx;
 
 use io_value::IOValue;
+use io_value::ShutterState;
 
 use tokio_modbus::prelude::Reader;
 use tokio_modbus::prelude::Writer;
@@ -34,6 +36,7 @@ pub async fn run(
     debug!("Output handling ids: {:?}", output_map.keys());
 
     let mut modbus_client = tcp::connect(remote_addr).await?;
+    let mut wait_task: HashMap<String, Instant> = HashMap::new();
 
     for (&id, context) in &mut output_map {
         match &context.output.kind {
@@ -42,19 +45,38 @@ pub async fn run(
                 let value = IOValue::Bool(read_var(&mut modbus_client, io.var).await?);
                 send_feedback(&tx_feedback, context, String::from(id), value)?;
             }
-            OutputKind::WOShutter(_) => {
+            OutputKind::WOShutter(io) => {
                 // Consider arbitrary value for shutter initialization
-                send_feedback(&tx_feedback, context, String::from(id), IOValue::Bool(true))?;
+                stop_shutter(&mut modbus_client, io).await?;
+                send_feedback(
+                    &tx_feedback,
+                    context,
+                    String::from(id),
+                    IOValue::Shutter(ShutterState::Up),
+                )?;
             }
             _ => {}
         }
     }
 
     loop {
-        if let Some(io_data) = rx.recv().await {
-            let id = io_data.id.clone();
+        tokio::select! {
+            io_data_opt = rx.recv() => {
+                if io_data_opt.is_none() {
+                    return Ok(())
+                }
+                let io_data = io_data_opt.unwrap();
+                let id = io_data.id.clone();
+                let ctx_opt = output_map.get_mut(id.as_str());
 
-            if let Some(ctx) = output_map.get_mut(id.as_str()) {
+                if ctx_opt.is_none() {
+                    warn!("Ignoring invalid ID {:?}", id);
+                    continue;
+                }
+
+                let ctx = ctx_opt.unwrap();
+                let current_value = ctx.value.read().unwrap().clone();
+
                 match &ctx.output.kind {
                     OutputKind::WODigital(io) => match io_data.value {
                         IOValue::Bool(value) => {
@@ -66,18 +88,60 @@ pub async fn run(
                         }
                     },
                     OutputKind::WOShutter(io) => {
-                        set_var(&mut modbus_client, io.var_up, true).await?;
-                        sleep(Duration::from_secs(3)).await;
-                        set_var(&mut modbus_client, io.var_up, false).await?;
+                        if current_value.is_some() && current_value.unwrap() == io_data.value {
+                            continue;
+                        }
+
+                        match &io_data.value {
+                            IOValue::Shutter(target_state) => {
+                                let (var_on, var_off) = match target_state {
+                                    ShutterState::Up => (io.var_up, io.var_down),
+                                    ShutterState::Down => (io.var_down, io.var_up),
+                                };
+
+                                set_var_off(&mut modbus_client, var_off).await?;
+                                set_var_on(&mut modbus_client, var_on).await?;
+                                wait_task.insert(id, Instant::now() + io.time);
+                            }
+                            _ => {
+                                warn!("Cannot handle value {:?} for {:?}", io_data.value, io)
+                            }
+                        }
                     }
                     _ => {
                         warn!("Output {:?} not implemented", io_data);
                     } // TODO
                 }
-            } else {
-                warn!("ID {:?} is not valid output", io_data);
+            },
+            _ = wait_for_instant(wait_task.values().min()) => {
+                let now = Instant::now();
+                for (id, instant) in &wait_task {
+                    if now > *instant {
+                        continue;
+                    }
+
+                    let ctx = output_map.get_mut(id.as_str()).unwrap();
+                    match &ctx.output.kind {
+                        OutputKind::WOShutter(io) => {
+                            stop_shutter(&mut modbus_client, io).await?;
+                            // TODO update state
+                        }
+                        _ => {
+                            warn!("Unexpected task");
+                        }
+                }
+
+                }
             }
         }
+    }
+}
+
+async fn wait_for_instant(opt_instant: Option<&Instant>) {
+    if opt_instant.is_none() {
+        futures::future::pending().await
+    } else {
+        tokio::time::sleep_until((*opt_instant.unwrap()).into()).await
     }
 }
 
@@ -98,6 +162,22 @@ async fn set_var(
     let address = u16::try_from(var | 0x1000)?;
     modbus_client.write_single_coil(address, value).await?;
 
+    Ok(())
+}
+async fn set_var_off(modbus_client: &mut dyn Writer, var: u32) -> Result<(), Box<dyn Error>> {
+    set_var(modbus_client, var, false).await
+}
+
+async fn set_var_on(modbus_client: &mut dyn Writer, var: u32) -> Result<(), Box<dyn Error>> {
+    set_var(modbus_client, var, true).await
+}
+
+async fn stop_shutter(
+    modbus_client: &mut dyn Writer,
+    io: &WagoIOUpDown,
+) -> Result<(), Box<dyn Error>> {
+    set_var(modbus_client, io.var_down, false).await?;
+    set_var(modbus_client, io.var_up, false).await?;
     Ok(())
 }
 
